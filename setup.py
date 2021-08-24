c317799aef11346da33233b371e5a24133fedff6
jonatasrenan <jonatas@dcc.ufmg.br>
2021-08-24 12:51:09 -0300

    1.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="itoptop",
-    version="1.0.8",
+    version="1.0.10",
     description="iTOP API Python Lib",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
